203cd68c39e085ce7d18b8877cb0d22281602660
NyFrdo <34854342+NyFrdo@users.noreply.github.com>
2021-07-15 18:13:37 +0800

    Update CommonOperationStep.java

--- CommonOperationStep.java
+++ CommonOperationStep.java
@@ -25,7 +25,7 @@ public class CommonOperationStep extends BaseSeleniumStep {
 
     public static void test(){
         System.out.println(testvar);System.out.println(testvar);
-        testvar = "Rez";
+        testvar = "Rez";testvar = "Rez";testvar = "Rez";testvar = "Rez";
 
     }
 
