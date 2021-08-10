05568c6b4a4ea0bc8c6d833bf5a952032da2eef3
nyfrdo <fradow@163.com>
2021-08-10 16:58:42 +0800

    vvv

--- CommonOperationStep.java
+++ CommonOperationStep.java
@@ -25,7 +25,7 @@ public class CommonOperationStep extends BaseSeleniumStep {
 
     public static void test(){
         System.out.println(testvar);System.out.println(testvar);
-        testvar = "test";testvar = "vvv";testvar = "test";testvar = "Rez";
+        testvar = "test";testvar = "ccc";testvar = "test";testvar = "Rez";
 		System.out.println(testvar);System.out.println(testvar);
     }
 
