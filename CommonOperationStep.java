d8e6d9600a94fc5c0f2966511460771e077453b5
nyfrdo <fradow@163.com>
2021-08-10 15:31:27 +0800

    test

--- CommonOperationStep.java
+++ CommonOperationStep.java
@@ -25,7 +25,7 @@ public class CommonOperationStep extends BaseSeleniumStep {
 
     public static void test(){
         System.out.println(testvar);System.out.println(testvar);
-        testvar = "VINCENT";testvar = "Rez";testvar = "sss";testvar = "Rez";
+        testvar = "test";testvar = "Rez";testvar = "sss";testvar = "Rez";
 		System.out.println(testvar);System.out.println(testvar);
     }
 
