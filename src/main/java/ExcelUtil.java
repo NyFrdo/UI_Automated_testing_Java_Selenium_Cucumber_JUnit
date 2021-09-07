46e1b78907912d8834c856deab00ebebdd4c5126
nyfrdo <fradow@163.com>
2021-09-07 15:33:51 +0800

    add excelutil and properties util

---
+++ src/main/java/ExcelUtil.java
@@ -0,0 +1,64 @@
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Assert;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+public class ExcelUtil {
+
+    public static final String configPath = PropertiesUtil.getKey("configPath");
+    public static final String configPathSec = PropertiesUtil.getKey("configPathSec");
+    public static final String defaultEnv = PropertiesUtil.getKey("Env");
+    public static final String defaultSheet = PropertiesUtil.getKey("defaultSheet");
+    public static Map<String,Integer> envMap = new HashMap<String,Integer>();
+    public static Map<String,Integer> columnMap = new HashMap<String,Integer>();
+    public static Sheet sheet ;
+//    public static String dbtype =
+//    public static String dburl =
+//    public static String dbport =
+//    public static String dbpwd =
+//    public static String dbInstance =
+
+    public static void fileExist (String path){
+        File f = new File(path);
+        Assert.assertTrue("File "+path+" not exist , please check", f.exists());
+    }
+
+    public static void readSheet (String path) {
+        try {
+            if (path.endsWith("xlsx")){
+                sheet = new XSSFWorkbook(new FileInputStream(path)).getSheet(defaultSheet);
+            }else sheet = new HSSFWorkbook(new FileInputStream(path)).getSheet(defaultSheet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Iterator<Cell> itColumnHeader = sheet.getRow(0).iterator();
+        Iterator<Cell> it = sheet.getRow(0).iterator();
+        int i = 1;
+        while (it.hasNext()){
+            envMap.put(it.next().toString(),i++);
+        }
+        for (int j=1;j<sheet.getLastRowNum();j++){
+            columnMap.put(sheet.getRow(j).getCell(0).toString(),j);
+        }
+//        System.out.println(envMap);
+//        System.out.println(columnMap);
+    }
+
+    public static String getCell (String columnName){
+        readSheet (configPath);
+        int columnIndex = envMap.get(defaultEnv);
+        int rowIndex = columnMap.get(columnName);
+        return sheet.getRow(rowIndex).getCell(columnIndex).toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getCell("dbpwd"));
+    }
+}
---
+++ src/main/java/PropertiesUtil.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesUtil {
+
+    public static String getKey(String key) {
+        Properties properties = new Properties();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("src/test/properties/config.properties"));
+            properties.load(bufferedReader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty(key);
+    }
+}
