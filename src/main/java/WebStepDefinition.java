32ca7965864319410a340f8d444b30be7d490d90
nyfrdo <fradow@163.com>
2021-09-09 18:38:33 +0800

    update

--- src/main/java/WebStepDefinition.java
+++ src/main/java/WebStepDefinition.java
@@ -1,4 +1,13 @@
-public class WebStepDefinition {
+import cucumber.api.java.en.Given;
+
+public class WebStepDefinition extends WebController{
+    public static WebElementDefinition we = new WebElementDefinition();
+    @Given("Login and check")
+    public void loginJustTest(){
+        login();
+        clickElement(we.avaButton);
+        clickElement(we.profilePerson);
+    }
 
 
 
