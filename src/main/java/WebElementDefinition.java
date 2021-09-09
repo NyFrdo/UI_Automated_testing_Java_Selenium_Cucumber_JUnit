7780c54b303ea26ba341fbb67613bd4994929234
nyfrdo <fradow@163.com>
2021-09-09 18:03:33 +0800

    add selenium cucumber

---
+++ src/main/java/WebElementDefinition.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.By;
+
+public class WebElementDefinition {
+    public static By avaButton =  By.xpath("//div//div[@class='toolbar-btn toolbar-btn-login csdn-toolbar-fl toolbar-subMenu-box']");
+    public static By profilePerson =  By.xpath("//li//i[contains(@class,'person')]");
+//    public static By avaButton =  By.xpath("");
+//    public static By avaButton =  By.xpath("");
+//    public static By avaButton =  By.xpath("");
+}
