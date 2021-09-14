906eee2e0fc8a1a1d77df334d7cc1b541ec4038e
nyfrdo <fradow@163.com>
2021-09-14 11:03:11 +0800

    fix runner issue

---
+++ src/main/java/StepDefinition.java
@@ -0,0 +1,11 @@
+import cucumber.api.java.en.Given;
+
+
+public class StepDefinition {
+    public static WebController controller = new WebController();
+
+    @Given("Login and check")
+    public void login(){
+
+    }
+}
