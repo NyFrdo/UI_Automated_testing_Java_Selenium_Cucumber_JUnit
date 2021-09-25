7dc6fbf5970feee9f3fc2c46b11ae7b6b1e21f68
nyfrdo <fradow@163.com>
2021-09-25 22:08:08 +0800

    update

--- src/main/java/elementDefiniton/WebElementDefinition.java
+++ src/main/java/elementDefiniton/WebElementDefinition.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.By;
 public class WebElementDefinition {
     public static By avaButton =  By.xpath("//div//div[@class='toolbar-btn toolbar-btn-login csdn-toolbar-fl toolbar-subMenu-box']");
     public static By profilePerson =  By.xpath("//li//i[contains(@class,'person')]");
+    public static By loginButton =  By.xpath("//div[contains(@class,'toolbar-btn-login')]");
+    public static By loginByAcccountAndPwdTab =  By.id("tabOne");
+    public static By userAccountInput =  By.id("all");
+    public static By userAccountPassword =  By.id("password-number");
+
 //    public static By avaButton =  By.xpath("");
 //    public static By avaButton =  By.xpath("");
 //    public static By avaButton =  By.xpath("");
