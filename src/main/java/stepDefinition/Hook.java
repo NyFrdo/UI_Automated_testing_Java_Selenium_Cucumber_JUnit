1e2a6a76b3122b09d5a6d7986c2c2838f14b89b7
nyfrdo <fradow@163.com>
2021-10-15 23:51:01 +0800

    update failed screencapture and apache io jar in dependency

--- src/main/java/stepDefinition/Hook.java
+++ src/main/java/stepDefinition/Hook.java
@@ -4,6 +4,7 @@ import controller.WebController;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -25,17 +26,12 @@ public class Hook extends WebController {
     public void after(Scenario scenario) throws Exception{
         if (scenario.isFailed()){
             File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            try {
-                String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmm"));
-                String screenshotName = "["+scenario.getId().split(";")[0]+"]"+"-"+scenario.getId().split(";")[1]+"-"+currentDateTime +".jpg";
-                File destFile = new File(System.getProperty("user.dir") + "/ScreenCapture/" + screenshotName);
-                FileUtil.copyFile(srcFile, destFile);
-            } catch (Exception e) {
-//            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmm"));
+            String screenshotName = "["+scenario.getId().split(";")[0]+"]"+"-"+scenario.getId().split(";")[1]+"-"+currentDateTime +".jpg";
+            File destFile = new File(System.getProperty("user.dir") + "/ScreenCapture/" + screenshotName);
+//                FileUtil.copyFile(srcFile, destFile);
+            FileUtils.copyFile(srcFile, destFile);
         }
-//        driver.
         driver.quit();
     }
 
