305c1d41d50684d8e79b4f4eb4d810f2d0c892ee
one <fradow@163.com>
2021-10-28 15:48:39 +0800

    fix hook not working issue

--- src/main/java/util/Hook.java
+++ src/main/java/util/Hook.java
@@ -2,6 +2,7 @@ package util;
 
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import qa.controller.WebController;
@@ -24,8 +25,8 @@ public class Hook extends WebController {
             String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmm"));
             String screenshotName = "["+scenario.getId().split(";")[0]+"]"+"-"+scenario.getId().split(";")[1]+"-"+currentDateTime +".jpg";
             File destFile = new File(System.getProperty("user.dir") + "/ScreenCapture/" + screenshotName);
-                FileUtil.copyFile(srcFile, destFile);
-//            FileUtils.copyFile(srcFile, destFile);
+//                FileUtil.copyFile(srcFile, destFile);
+            FileUtils.copyFile(srcFile, destFile);
         }
         driver.quit();
     }
