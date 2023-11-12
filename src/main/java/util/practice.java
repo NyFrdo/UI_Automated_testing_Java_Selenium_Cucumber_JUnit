62726fb8e8be41355b2ca3df2fe4d21e4ac64f67
Rez <fradow@163.com>
2023-11-12 20:06:39 +0800

    further update script for fixing issue like 12.1 12.1.1

--- src/main/java/util/practice.java
+++ src/main/java/util/practice.java
@@ -6,13 +6,14 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class practice {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         //find latest version number
         List<String> a = Lists.newArrayList(
-                "Release-1.3", "Release-12.05.1", "Release-3.1", "Release-1.4.3", "Release-1.4.4", "Release-2.0.1", "Release-2.1.1", "Release-12.05.0788"
+                "Release-1.3", "Release-12.05.235", "Release-3.1", "Release-1.4", "Release-1.4.4", "Release-12.0.1", "Release-12.2.1", "Release-12.2.1"
+                , "Release-12.05.235.1"
 
-//               "Release-1.3","Release-3.3","Release-12.05","Release-12.2"
+//               "Release-22.05","Release-12.2"
 
 
         );
@@ -27,36 +28,39 @@ public class practice {
 
     }
 
-    public static String getheadandsort(List<String> ab, int coun) {
-        List<BigDecimal> loopint1 = Lists.newArrayList();
-        List<BigDecimal> loopint2 = Lists.newArrayList();
+    public static String getheadandsort(List<String> ab, int coun)  {
+        List<Integer> notsortedversion = Lists.newArrayList();
+        List<Integer> sortedversion = Lists.newArrayList();
         for (int k = 0; k < ab.size(); k++) {
-            String buildnum = ab.get(k).split("\\.")[coun];
-            if (buildnum.startsWith("0")) {
-                loopint1.add(new BigDecimal("0." + buildnum));
-            } else loopint1.add(new BigDecimal(buildnum));
-
+            try{
+                String buildnum = ab.get(k).split("\\.")[coun];
+                notsortedversion.add(Integer.parseInt(buildnum));
+            }catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("because dont have num at this digit , set as 0 ");
+                notsortedversion.add(0);
+            }
         }
-        loopint2.addAll(loopint1);
-//        loopint1.sort(Comparator.naturalOrder()); using inside utility to sort
-        loopint1 = bubblesort(loopint1);
+
+        sortedversion.addAll(notsortedversion);
+//        notsortedversion.sort(Comparator.naturalOrder()); using inside utility to sort
+
+        sortedversion = bubblesort(sortedversion);
 
 //        loopint1.add(new BigDecimal("3.0"));
 
         List<Integer> index = Lists.newArrayList();
-        for (int i = 0; i < loopint2.size(); i++) {
-            if (loopint2.get(i).compareTo(loopint1.get(loopint1.size() - 1)) == 0) {
+        for (int i = 0; i < sortedversion.size(); i++) {
+            if (notsortedversion.get(i).equals(sortedversion.get(sortedversion.size()-1)) ) {
                 index.add(i);
             }
         }
 
 
-        coun++;
-
         if (index.size() == 1) {
             return ab.get(index.get(0));
         }
 
+        coun++;
         List<String> bc = Lists.newArrayList();
         index.forEach(v -> {
             bc.add(ab.get(v));
@@ -67,15 +71,20 @@ public class practice {
 
     }
 
-    public static List<BigDecimal> bubblesort(List<BigDecimal> ab) {
-        BigDecimal[] a = ab.toArray(new BigDecimal[0]);
+    public static List<Integer> bubblesort(List<Integer> ab) {
+        Integer[] a = ab.toArray(new Integer[0]);
         for (int i = 0; i < a.length - 1; i++) {
-            for (int j = i + 1; j < a.length - 1; j++) {
-                if (a[i].compareTo(a[j]) == 1) {
-                    BigDecimal temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+            boolean flag = true;
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j]>a[j+1]) {
+                    Integer temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    flag = false;
                 }
+            }
+            if(flag){
+                break;
             }
 
         }
